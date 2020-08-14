0ef9a1d0874a96773ea5e94d7ea5f73a3c74fee6
NestorGon <69483489+NestorGon@users.noreply.github.com>
2020-08-13 19:50:17 -0500

    Métodos completos

--- src/view/View.java
+++ src/view/View.java
@@ -13,6 +13,9 @@ public class View
 	    	
 	    }
 	    
+	    /**
+	     * Imprime el menú del programa en consola
+	     */
 		public void printMenu()
 		{
 			System.out.println("1. Crear Arreglo Dinamico de Strings");
@@ -24,12 +27,20 @@ public class View
 			System.out.println("Dar el numero de opcion a resolver, luego oprimir tecla Return: (e.g., 1):");
 		}
 
+		/**
+		 * Imprime en consola el mensaje ingresado por parámetro
+		 * @param mensaje - el mensaje que se va a imprimir
+		 */
 		public void printMessage(String mensaje) 
 		{
 
 			System.out.println(mensaje);
 		}		
 		
+		/**
+		 * Imprime el arreglo del modelo
+		 * @param modelo - el modelo con la información a imprimir
+		 */
 		public void printModelo(Modelo modelo)
 		{
 			IArregloDinamico arreglo = modelo.darArreglo();
